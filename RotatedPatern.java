bbc572f8bcca3c18c3370f83bb1bf7f275e52300
TEJAS GAJANAN KAWARKHE <112716745+tejas2601@users.noreply.github.com>
2025-07-03 22:49:41 +0530

    Create RotatedPatern.java

---
+++ RotatedPatern.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    int n=5;
+	    int space=n/2,star=1;
+		for(int i=0;i<n;i++){
+		    for(int j=0;j<space;j++){
+		        System.out.print("  ");
+		    }
+		    for(int j=0;j<star;j++){
+		        System.out.print("*");
+		    }
+		    if(i<n/2){
+		        star=star+2;
+		        space--;
+		    }else{
+		        star-=2;
+		        space++;
+		    }
+		    System.out.println();
+		}
+	}
+}
